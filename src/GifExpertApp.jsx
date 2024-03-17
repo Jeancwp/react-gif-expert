78e4fbfdb4da358b9163653e16c1db927406e13f
coopsermul <cooperativasermul@gmail.com>
2024-03-16 20:48:09 -0500

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch']);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    //console.log(newCategory);
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        //setCategories={setCategories}
+        onNewCategory={(value) => onAddCategory(value)}
+      />
+
+      {/* Listado de Gif */}
+
+      {/* Gif Item */}
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
